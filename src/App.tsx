f6a6aba0b80252934b8958bad7020d933aa83abf
Evgenii <evtatdevelop@gmail.com>
2022-05-15 15:01:20 +0700

    Added main pakages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './rest.css';
-import './App.css';
+import './App.module.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+
+  const element = <FontAwesomeIcon icon={faCoffee} />
+
   return (
     <div className="App">
       <main>
-        <h1>Organizer</h1>
+        <h1>Organizer {element}</h1>
       </main>
     </div>
   );
